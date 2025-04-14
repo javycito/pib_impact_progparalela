dc6a77798fee5c23d44f91f9d9dc5693e2ba80dc
TheDahakaRD <161846960+TheDahakaRD@users.noreply.github.com>
2025-04-14 19:49:23 -0400

    Create SectorEconomico.cs
    Se creo la clase Sector Economico

---
+++ src/Models/SectorEconomico.cs
@@ -0,0 +1,9 @@
+namespace PBImpact.Models
+{
+    public class SectorEconomico
+    {
+        public string Nombre { get; set; }
+        // Ejemplo de propiedad adicional
+        public decimal Crecimiento { get; set; }
+    }
+}
