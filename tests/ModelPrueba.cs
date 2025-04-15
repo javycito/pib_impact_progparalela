c3f7a0d258591bf1827f6d449284dc8ad24552ec
javycito <enjherjavier01@gmail.com>
2025-04-14 23:36:41 -0400

    Agregada prueba unitaria inicial para usar clase Pais que no está de manera oficial\nCo-authored-by: EnjherJavierAgueroOvalles

---
+++ tests/ModelPrueba.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using PBImpact.Models;
+
+namespace PBImpact.Tests
+{
+    public class PaisesPrueba
+    {
+        private Pais CrearPaisPrueba()
+        {
+            return new Pais { Nombre = "Brasil", PIB = 1000.50m };
+        }
+
+        [Fact]
+        public void DeberiaRetornarNombreCorrecto()
+        {
+            var pais = CrearPaisPrueba();
+            var nombre = pais.Nombre;
+            Assert.Equal("Brasil", nombre);
+        }
+
+        [Fact]
+        public void DeberiaRetornarPIBCorrecto()
+        {
+            var pais = CrearPaisPrueba();
+            var pib = pais.PIB;
+            Assert.Equal(1000m, pib);
+        }
+    }
+}
